97872e340f7aa01ac8497f54842f08b36b954584
Sebastian Gpe Tanori Ruiz <tanoriruizs@gmail.com>
2023-08-22 19:31:21 -0700

    Falta agregar la funcion ventas y los likes y dislikes

---
+++ ApiGatewayBlazor/Client/Models/Entidad/CarritoCompra.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiGatewayBlazor.Client.Models.Entidad
+{
+    public class CarritoCompra
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ ApiGatewayBlazor/Server/Models/CarritoCompra.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ApiGatewayBlazor.Client.Models.Entidad
+{
+    public class CarritoCompra
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public Producto Producto { get; internal set; }
+    }
+}
