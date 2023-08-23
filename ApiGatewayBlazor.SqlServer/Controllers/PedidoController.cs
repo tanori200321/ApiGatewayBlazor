58ec7794d702f2aafc4b645a062f1f8d2cfa77a1
Sebastian Gpe Tanori Ruiz <tanoriruizs@gmail.com>
2023-08-22 18:32:23 -0700

    Cambios

---
+++ ApiGatewayBlazor.SqlServer/Controllers/PedidoController.cs
@@ -0,0 +1,21 @@
+﻿using ApiGatewayBlazor.SqlServer.Models;
+using ApiGatewayBlazor.SqlServer.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGatewayBlazor.SqlServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PedidoController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PedidoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+    }
+}
